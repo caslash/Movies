f781856c9fc74db3ee4aff6ed37151c76f9b4d74
Cameron Slash <calexslash@aol.com>
2023-04-30 20:11:00 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Cameron Slash on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
